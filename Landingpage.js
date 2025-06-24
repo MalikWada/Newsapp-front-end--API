10cbd9cb721bccd0dd6962037236589203f90ec4
Umar Wada <umarvadaa@gmail.com>
2025-06-24 15:43:34 +0500

    Add files via upload

---
+++ Landingpage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LandingPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to NewsApp</h1>
+      <p>This is the landing page of your React project.</p>
+    </div>
+  );
+}
